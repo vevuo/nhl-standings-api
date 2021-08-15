1e7b105603835c50100b560c0a725e7c08690679
vevuo <vesa.vuoristo@gmail.com>
2021-08-15 22:44:17 +0300

    Adding example endpoints

--- src/main.py
+++ src/main.py
@@ -4,5 +4,17 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "NHL standings API"}
+async def index():
+    return {"message": "NHL standings API. See /docs or /redoc"}
+
+
+@app.get("/teams")
+async def list_all_teams():
+    return [
+        {"team_name": "Montreal Canadiens"}
+    ]
+
+
+@app.get("/team/{team_id}")
+async def list_team_details_by_id(team_id: int):
+    return {"team_id": team_id}
